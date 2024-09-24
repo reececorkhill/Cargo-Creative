0bdfdaf6c85e834fef8e8bab9319e5b0d6b04e91
Reece Corkhill <reece@smartit-webdevelopment.com>
2024-09-25 00:28:29 +0100

    Main Header now finished with working slide pill.

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const mainButton = document.getElementById("show-main-btn");
+const secondaryButton = document.getElementById("show-secondary-btn");
+
 window.addEventListener("scroll", function() {
     
     const navbar = document.getElementById("navbar");
@@ -7,4 +10,28 @@ window.addEventListener("scroll", function() {
     } else {
       navbar.classList.remove("scrolled");
     }
-});   
+});
+
+mainButton.addEventListener("click", function() {
+
+  const mainBtn = document.getElementById("show-main-btn");
+  const secondaryBtn = document.getElementById("show-secondary-btn");
+
+  mainBtn.classList.remove('not-selected');
+  mainBtn.classList.add('selected');
+  secondaryBtn.classList.remove('selected');
+  secondaryBtn.classList.add('not-selected');
+
+});
+
+secondaryButton.addEventListener("click", function() {
+
+  const mainBtn = document.getElementById("show-main-btn");
+  const secondaryBtn = document.getElementById("show-secondary-btn");
+
+  mainBtn.classList.remove('selected');
+  mainBtn.classList.add('not-selected');
+  secondaryBtn.classList.remove('not-selected');
+  secondaryBtn.classList.add('selected')
+
+});
