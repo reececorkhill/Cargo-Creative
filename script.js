9fd3af0c9a53f8f52436d9afbf63c7baa3a517ab
Reece Corkhill <reece@smartit-webdevelopment.com>
2024-10-04 00:51:42 +0100

    Finished header and click listener.

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 const mainButton = document.getElementById("show-main-btn");
 const secondaryButton = document.getElementById("show-secondary-btn");
+const header = document.getElementById("header");
+const headerTitle = document.getElementById("header-title");
+const headerSubtitle = document.getElementById("header-subtitle");
+const headerTagline = document.getElementById("header-tagline");
+const headerh2 = document.getElementById("header-h2");
 
 window.addEventListener("scroll", function() {
     
     const navbar = document.getElementById("navbar");
 
-    if (window.scrollY > 50) {  // Change background when scrolled down by 50px
+    if (window.scrollY > 50) {
       navbar.classList.add("scrolled");
     } else {
       navbar.classList.remove("scrolled");
@@ -22,6 +27,13 @@ mainButton.addEventListener("click", function() {
   secondaryBtn.classList.remove('selected');
   secondaryBtn.classList.add('not-selected');
 
+  header.style.backgroundImage = 'linear-gradient(to left, rgba(255, 255, 255, 0.025), rgba(0, 0, 59, 0.695)), url("/assets/1106129fd572fda500f591c40b284e6f")';
+  headerTitle.textContent = "Main Header";
+  headerTitle.style.fontSize = "142px";
+  headerSubtitle.textContent = "This is our";
+  headerTagline.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.Pellentesque fermentum libero velit, sit amet suscipit mauris elementum id. Praesent interdum a ante ut dapibus.";
+  headerh2.innerHTML = 'Find out more<i class="fa-sharp fa-solid fa-circle-arrow-right fa-2xl" id="find-out-arrow"></i>';
+
 });
 
 secondaryButton.addEventListener("click", function() {
@@ -32,6 +44,13 @@ secondaryButton.addEventListener("click", function() {
   mainBtn.classList.remove('selected');
   mainBtn.classList.add('not-selected');
   secondaryBtn.classList.remove('not-selected');
-  secondaryBtn.classList.add('selected')
+  secondaryBtn.classList.add('selected');
+
+  header.style.backgroundImage = 'linear-gradient(to left, rgba(255, 255, 255, 0.025), rgba(0, 0, 59, 0.695)), url("/assets/brooke-cagle-NoRsyXm.png")';
+  headerTitle.textContent = "Secondary Header";
+  headerTitle.style.fontSize = "118px";
+  headerSubtitle.textContent = "And this is a";
+  headerTagline.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.Pellentesque fermentum libero velit, sit amet suscipit mauris elementum id. Praesent interdum a ante ut dapibus.";
+  headerh2.innerHTML = 'Find out more<i class="fa-sharp fa-solid fa-circle-arrow-right fa-2xl" id="find-out-arrow"></i>';
 
 });
